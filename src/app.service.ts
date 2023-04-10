11469a0da73437791a08a63e11818e21abf125e8
Wesley Sousa <wesleytuck@oulook.com>
2023-04-10 04:01:07 -0300

    clean

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { JsreportService } from "./jsreport.service";
 import ReportDataset from "./report-models/datasets/base.dataset";
-import dataset from "./dataset";
+import data from "./report-models/database/data";
 import handlebarHelpers from "./report-models/helpers/handlebar.helpers";
 
 const TEMPLATES_PATH = "/assets/templates";
@@ -15,7 +15,7 @@ async generatePdf(){
 
     return this.jsreportService.generateReport(
       `${TEMPLATES_PATH}/reports/person.html`,
-      new ReportDataset(dataset, 10),
+      new ReportDataset(data, 10),
       handlebarHelpers
     )
   }
